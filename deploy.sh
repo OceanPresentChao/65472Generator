b37db48d640bcbe910cbc7af56c870b9a203a4a9
OceanPresent <oceanpresent@163.com>
2022-05-01 00:01:25 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd dist
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:OceanPresent/65472generator.git main:gh-pages
--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,6 @@ export default defineConfig({
   },
   server: {
     open: true
-  }
+  },
+  base: '/65472Generator/'
 })
